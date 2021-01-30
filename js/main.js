d5b284a54812b5cf9e5829b72b90b3c715dad3d2
Daniel Argaman <Argaman.at.work@gmail.com>
2021-01-30 16:21:11 +0200

    Start

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const ARTIST_INPUT = document.getElementById("input-artist");
+const ALBUM_INPUT = document.getElementById("input-album");
+const ALBUM_IMAGE = document.getElementById("album-img");
+const ALBUM_NAME = document.getElementById("album-name");
+const ALBUM_DESCRIPTION = document.getElementById("album-description");
+const SUBMIT = document.getElementById("submit");
+const LAMBDA_URL = "https://ukc8unbj0d.execute-api.us-east-1.amazonaws.com/nesh";
+
+
+[ARTIST_INPUT, ALBUM_INPUT].forEach( (element) => {
+  element.addEventListener("change", () => {
+    if (element.value) {
+      element.classList.add('is-valid'); 
+      element.classList.remove('is-invalid');
+    } else {
+      element.classList.add('is-invalid');
+      element.classList.remove('is-valid');  
+    } 
+  })
+});
+
+SUBMIT.addEventListener("click", compileData);
+
+async function compileData() {
+  const DATA = [ARTIST_INPUT.value, ALBUM_INPUT.value]
+  if (ARTIST_INPUT.value && ALBUM_INPUT.value) {
+    console.log("Invoked Lambda")
+    const albumArray = await invokeLambda(DATA, LAMBDA_URL);
+    console.log(albumArray);
+    ALBUM_IMAGE.src = albumArray.url;
+    ALBUM_NAME.innerHTML = albumArray.description;
+    ALBUM_DESCRIPTION.innerHTML = albumArray.info;
+  }
+}
+
+async function invokeLambda(data = {}, url = LAMBDA_URL) {
+  const response = await fetch(url, {
+    method: 'POST', 
+    body: JSON.stringify(data) 
+  });
+  return response.json();
+}
+
+
+ 
